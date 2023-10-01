ef445ca71a8aec774a03f64a54893762d3122aa3
Shampra <16141040+Shampra@users.noreply.github.com>
2023-10-01 16:52:52 +0200

    Files format and enex test

--- Enex_to_json/models/mime.py
+++ Enex_to_json/models/mime.py
@@ -1,82 +1,80 @@
-PDF = "application/pdf"
+# mime.py
 
-IMAGES = [
-    "image/jpeg",
-    "image/png",
-    "image/svg+xml",
-    "image/webp",
-    "image/gif",
-    "image/avif",
-    "image/apng",
-    "image/bmp"
-]
-
-TEXTS = [
-    "text/plain",
-    "text/csv",
-    "text/html",
-    "application/msword",
-    "text/css",
-    "application/x-csh",
-    "application/msword",
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.template",
-    "application/json",
-    "application/ld+json",
-    "text/comma-separated-values"
-]
-
-VIDEOS = [
-    "video/mp4",
-    "video/3gpp",
-    "video/3gpp2",
-    "video/H261",
-    "video/H263",
-    "video/mpv",
-    "video/ogg",
-    "video/x-msvideo"
-]
-
-AUDIOS = [
-    "audio/mp4",
-    "audio/wave",
-    "audio/wav",
-    "audio/x-wav",
-    "audio/webm",
-    "audio/ogg",
-    "audio/aac",
-    "audio/ac3",
-    "audio/mpeg",
-    "audio/midi",
-    "audio/x-midi",
-    "audio/m4a",
-    "audio/mp3",
-    "audio/x-flac",
-    "audio/flac"
-]
-
-ARCHIVE = [
-    "application/zip",
-    "application/zlib",
-    "application/x-rar-compressed",
-    "application/octet-stream",
-    "application/x-zip-compressed",
-    "multipart/x-zip",
-    "application/x-bzip",
-    "application/x-bzip2",
-    "application/gzip",
-    "application/vnd.rar",
-    "application/java-archive",
-    "application/x-tar",
-    "application/x-7z-compressed"
-]
-
-TABLE = [
-    "application/vnd.ms-excel",
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-]
-
-PRESENTATION = [
-    "application/vnd.ms-powerpoint",
-    "application/vnd.openxmlformats-officedocument.presentationml.presentation"
-]
+# Liste dynamique de catégories de fichiers et de leurs types MIME correspondants
+TYPE = {
+    "PDF": ["application/pdf"],
+    "Image": [
+        "image/jpeg",
+        "image/png",
+        "image/svg+xml",
+        "image/webp",
+        "image/gif",
+        "image/avif",
+        "image/apng",
+        "image/bmp"
+    ],
+    "Text": [
+        "text/plain",
+        "text/csv",
+        "text/html",
+        "application/msword",
+        "text/css",
+        "application/x-csh",
+        "application/msword",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.template",
+        "application/json",
+        "application/ld+json",
+        "text/comma-separated-values"
+    ],
+    "Video": [
+        "video/mp4",
+        "video/3gpp",
+        "video/3gpp2",
+        "video/H261",
+        "video/H263",
+        "video/mpv",
+        "video/ogg",
+        "video/x-msvideo"
+    ],
+    "Audio": [
+        "audio/mp4",
+        "audio/wave",
+        "audio/wav",
+        "audio/x-wav",
+        "audio/webm",
+        "audio/ogg",
+        "audio/aac",
+        "audio/ac3",
+        "audio/mpeg",
+        "audio/midi",
+        "audio/x-midi",
+        "audio/m4a",
+        "audio/mp3",
+        "audio/x-flac",
+        "audio/flac"
+    ],
+    "ARCHIVE": [
+        "application/zip",
+        "application/zlib",
+        "application/x-rar-compressed",
+        "application/octet-stream",
+        "application/x-zip-compressed",
+        "multipart/x-zip",
+        "application/x-bzip",
+        "application/x-bzip2",
+        "application/gzip",
+        "application/vnd.rar",
+        "application/java-archive",
+        "application/x-tar",
+        "application/x-7z-compressed"
+    ],
+    "TABLE": [
+        "application/vnd.ms-excel",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    ],
+    "PRESENTATION": [
+        "application/vnd.ms-powerpoint",
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    ],
+}
