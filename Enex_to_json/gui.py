a8f1e51fe05b36f908c0f59a41237b12f4947bd5
Shampra <16141040+Shampra@users.noreply.github.com>
2024-03-20 15:19:14 +0100

    Remove log

--- Enex_to_json/gui.py
+++ Enex_to_json/gui.py
@@ -97,7 +97,6 @@ class Interface:
         
 def resource_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-    print(f"Vérif icone : {os.path.join(base_path, relative_path)}")
     return os.path.join(base_path, relative_path)
 
 def main(version):
